1d670d9a8ede32870924de82019ff22f583ca350
Ricardo Zuno <zunosan.ricardo506@gmail.com>
2025-04-11 02:15:32 -0600

    Finalizando proyecto

--- src/modules/landing/pages/HomePage.vue
+++ src/modules/landing/pages/HomePage.vue
@@ -4,7 +4,11 @@
     <p class="mx-auto max-w-[600px] text-gray-500 md:text-xl">
       Lorem ipsum dolor sit amet, consectetur adipiscing elit.
     </p>
+
+    <p>Counter: {{ counter }}</p>
+
+    <button @click="counter++">+1</button>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script lang="ts" src="./HomePage.ts"></script>
